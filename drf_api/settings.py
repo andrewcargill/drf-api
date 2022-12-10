c35c82f72600c3809b0b04630460d68e97df8b45
andrewcargill <andycargill01@aol.com>
2022-12-10 07:37:19 +0000

    edit cors code from master doc

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,19 +37,31 @@ ALLOWED_HOSTS = [
 ]
 
 
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN')
+#     ]
+#     print(os.environ.get('CORS_ALLOWED_ORIGINS'))
+
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+#     ]
+#     print(os.environ.get('CORS_ALLOWED_ORIGIN_REGEXES'))
+
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-    print(os.environ.get('CORS_ALLOWED_ORIGINS'))
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-    print(os.environ.get('CORS_ALLOWED_ORIGIN_REGEXES'))
-
 
 CORS_ORIGIN_ALLOW_ALL = True
 
